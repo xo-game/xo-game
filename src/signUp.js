380b87100ea94b0977734e7a8a64ac919fc23df7
Allan Vega <vega.allan11@gmail.com>
2024-02-27 15:32:23 -0500

    change the language to english

--- src/signUp.js
+++ src/signUp.js
@@ -1,48 +1,30 @@
 /*
 to custom the message of an input required
 */
-function validarCampoRequerido(event) {
+function verifyRequiredField(event) {
     //get the reference of the field when occur the event
-    const campo = event.target;
+    const field = event.target;
     //get the name of the field where the event occur
-    const nombreCampo = campo.getAttribute('name');
+    const fieldName = field.getAttribute('name');
 
     //verify if the field is empty or only contain blank spaces
-    if (!campo.value.trim()) {
+    if (!field.value.trim()) {
         //if it is empty
-        campo.setCustomValidity(`Need to complete the field: ${nombreCampo}`);
+        field.setCustomValidity(`Need to complete the field: ${fieldName}`);
         //if is not empty, restore the validate message
     } else {
-        campo.setCustomValidity('');
+        field.setCustomValidity('');
     }
 }
 
 //select all the required field of the form
-const camposRequeridos = document.querySelectorAll('form input[required]');
+const requiredField = document.querySelectorAll('form input[required]');
 //iterate over the required fields and add an event invalid
-camposRequeridos.forEach(function (campo) {
-    campo.addEventListener('invalid', validarCampoRequerido);
+requiredField.forEach(function (campo) {
+    campo.addEventListener('invalid', verifyRequiredField);
 });
 
 /***************/
-
-/*
-To verify if the email ends with @narcis.com
-*/
-function verifyEmail(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value.toLowerCase();
-
-    if(email.endsWith('@narcis.com')) {
-        alert('The email exist');
-        alert(email);
-        verifyPasswordEquality();
-    }
-    else {
-        alert('You need to insert a @narcis.com in email field')
-    }
-}
-
 /*
 Verify the password equality
 */
@@ -58,6 +40,26 @@ function verifyPasswordEquality() {
     }
 }
 
+/***************/
+/*
+To verify if the email ends with @narcis.com
+*/
+
+function verifyEmail(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value.toLowerCase();
+
+    if(email.endsWith('@narcis.com')) {
+        alert('The email exist');
+        alert(email);
+        verifyPasswordEquality();
+    }
+    else {
+        alert('You need to insert a @narcis.com in email field')
+    }
+}
+
+/***************/
 /*
 All submit functiions
 */
